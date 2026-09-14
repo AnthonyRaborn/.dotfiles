01b24b97ee9d775a158683b724bf4eb72662e6da
Anthony W. Raborn <anthony@raborndecisionsciences.com>
2026-09-14 13:11:15 -0400

    Fixed spacing for spaces, spaces backgrounds, and both bars

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 SPACE_ID=$1
 
-# Hardcoded, since this script runs as its own process and
-# does not inherit variables exported in sketchybarrc
 CYAN=0xff95E6CB
 ORANGE=0xffFF8F40
+INACTIVE_BG=0x30ffffff
+ACTIVE_BG=0xffFF8F40
 
 FOCUSED=$(yabai -m query --spaces --space "$SPACE_ID" | grep '"has-focus"' | grep -c true)
 
 if [ "$FOCUSED" = "1" ]; then
-  sketchybar --set space.$SPACE_ID icon.color=$ORANGE
+  sketchybar --set space.$SPACE_ID icon.color=0xff0A0E14 background.color=$ACTIVE_BG
 else
-  sketchybar --set space.$SPACE_ID icon.color=$CYAN
+  sketchybar --set space.$SPACE_ID icon.color=$CYAN background.color=$INACTIVE_BG
 fi
